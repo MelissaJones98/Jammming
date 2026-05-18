3734b2700ae702f5ead5ab3fdedcb2d5e3a6cc90
MelissaJones98 <melissajaynejones.16@gmail.com>
2026-05-18 15:28:05 +0100

    added some basic structure and ensured that the first React component is rendering in the browser

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Track from './components/Track';
+
+function App() {
+  return (
+    <div>
+      <Track />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Track.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Track() {
+  return (
+    <div>
+      <h3>Song Name</h3>
+      <p>Artist | Album</p>
+      <button>+</button>
+    </div>
+  );
+}
+
+export default Track;
