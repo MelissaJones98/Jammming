87cb88bf479b2a1819e3ae7d172c4c8d802035dc
MelissaJones98 <melissajaynejones.16@gmail.com>
2026-05-19 13:08:26 +0100

    Spotify no longer supports the implicit grant flow previously used - updated to PKCE

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,45 +1,74 @@
 const clientId = 'e060cddc20e6430e93be8d154880121f';
 const redirectUri = 'https://localhost:5173';
 
-let accessToken;
+async function generateCodeVerifier() {
+  const array = new Uint8Array(32);
+  crypto.getRandomValues(array);
+  return btoa(String.fromCharCode(...array))
+    .replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+async function generateCodeChallenge(verifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(verifier);
+  const digest = await crypto.subtle.digest('SHA-256', data);
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
 
 const Spotify = {
-  getAccessToken() {
-    if (accessToken) {
-      return accessToken;
-    }
-    
-    const tokenInUrl = window.location.href.match(/access_token=([^&]*)/);
-    const expiryTime = window.location.href.match(/expires_in=([^&]*)/);
+  async getAccessToken() {
+    const token = localStorage.getItem('spotify_access_token');
+    const expiry = localStorage.getItem('spotify_token_expiry');
 
-    if (tokenInUrl && expiryTime) {
-      accessToken = tokenInUrl[1];
-      window.setTimeout(() => accessToken = '', Number(expiryTime[1]) * 1000);
-      window.history.pushState('Access Token', null, '/');
-      return accessToken;
+    if (token && expiry && Date.now() < Number(expiry)) {
+      return token;
     }
 
-    window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+    const code = new URLSearchParams(window.location.search).get('code');
+
+    if (code) {
+      const verifier = localStorage.getItem('spotify_code_verifier');
+      const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({
+          client_id: clientId,
+          grant_type: 'authorization_code',
+          code,
+          redirect_uri: redirectUri,
+          code_verifier: verifier,
+        }),
+      });
+      const data = await response.json();
+      localStorage.setItem('spotify_access_token', data.access_token);
+      localStorage.setItem('spotify_token_expiry', Date.now() + data.expires_in * 1000);
+      window.history.pushState({}, '', '/');
+      return data.access_token;
+    }
+
+    const verifier = await generateCodeVerifier();
+    const challenge = await generateCodeChallenge(verifier);
+    localStorage.setItem('spotify_code_verifier', verifier);
+
+    window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=playlist-modify-public&code_challenge_method=S256&code_challenge=${challenge}`;
   },
 
-    search(term) {
-        const token = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-            headers: { Authorization: `Bearer ${token}` }
-        }).then(response => response.json())
-        .then(jsonResponse => {
-            if (!jsonResponse.tracks) {
-                return [];
-            }
-            return jsonResponse.tracks.items.map(track => ({
-                id: track.id,
-                name: track.name,
-                artist: track.artists[0].name,
-                album: track.album.name,
-                uri: track.uri
-            }));
-        });
-    }
+  async search(term) {
+    const token = await Spotify.getAccessToken();
+    const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    const data = await response.json();
+    if (!data.tracks) return [];
+    return data.tracks.items.map(track => ({
+      id: track.id,
+      name: track.name,
+      artist: track.artists[0].name,
+      album: track.album.name,
+      uri: track.uri
+    }));
+  }
 };
 
 export default Spotify;
